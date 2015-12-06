88ef1108f48b269b403b34668d612b2935e6154f
rabit1 <rabit1@mailinator.com>
2015-12-06 19:08:17 +0700

    pi bin

--- updatedynu.sh
+++ updatedynu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
-nowip=$(curl -s http://whatismyip.akamai.com)
+#nowip=$(curl -s http://whatismyip.akamai.com)
+nowip=$(wget -qO- http://ipecho.net/plain)
 echo $nowip
 
 #read -r lastip<currentip
@@ -8,17 +9,17 @@ lastip=$(resolveip -s big.dynu.com)
 #echo "lastip: "
 echo $lastip
 
-if [ $nowip != $lastip ]
+if [ "$nowip" != "$lastip" ]
 then
 	echo "Not Same"
 	wget "https://api.dynu.com/nic/update?hostname=big.dynu.com&myip="$nowip"&username=rabit1&password=rabit12"
 	curl -s http://whatismyip.akamai.com > currentip
 	# wget -qO- http://ipecho.net/plain
 	# wget -qO- http://ipecho.net/plain > currentip
-# else
-	# echo "Same"
+else
+	echo "Same"
 fi
 
 rm -fr update?hostname*
-
+date > lastrun
 
